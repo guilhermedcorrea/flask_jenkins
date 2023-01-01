1509512355f516685b1f1b410c6f4df0821dec05
guilhermedcorrea <guilherme.d.correa@live.com>
2023-01-01 07:25:42 -0300

    app

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app import app
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', threaded=True, port=7005, debug=True)
+    app.run(host='0.0.0.0', threaded=True, port=5000, debug=True)
